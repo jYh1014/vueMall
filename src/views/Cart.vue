1f26015d1606c6ce4d352c9981035e580b1e3640
jYh1014 <3200371428@qq.com>
2017-10-30 10:20:30 +0800

    add address page

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -122,7 +122,7 @@
                 Item total: <span class="total-price">{{totalPrice | currency('¥')}}</span>
               </div>
               <div class="btn-wrap">
-                <a class="btn btn--red" >Checkout</a>
+                <a class="btn btn--red" :class="{'btn--dis':checkedCount == 0}" @click="checkOut">Checkout</a>
               </div>
             </div>
           </div>
@@ -264,6 +264,13 @@
                 .then(response => {
 
                 })
+            },
+            checkOut(){
+              if(this.checkedCount > 0){
+                this.$router.push({
+                  path:'/address'
+                })
+              }
             }
         }
     }
