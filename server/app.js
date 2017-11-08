49de652c8be6b6cb91dfbdddb519486db70c8c56
jiangying <3200371428@qq.com>
2017-11-08 21:01:32 +0800

    333

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 //拦截用户未登录
 app.use(function(req,res,next){
