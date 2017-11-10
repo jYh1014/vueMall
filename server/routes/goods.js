fb1aa82f8cbfaf895aeec363d07af548d24c7648
jiangying <3200371428@qq.com>
2017-11-10 09:58:16 +0800

    2

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -5,19 +5,6 @@ mongoose.Promise = require('bluebird')
 let Goods = require('../models/goods')
 let User = require('../models/user')
 
-// mongoose.connect('mongodb://127.0.0.1:27017/db_mall')
-// mongoose.connection.on('connected',function(){
-//     console.log('connect success')
-// })
-
-// mongoose.connection.on('disconnected',function(){
-//     console.log('disconnect')
-// })
-
-// mongoose.connection.on('error',function(){
-//     console.log('connect fail')
-// })
-
 //查询商品数据
 router.get('/list',function(req,res,next){
     
